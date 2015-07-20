81344e8db7adfc91d054979778f7350c4515085b
Ronald J. Lugge III <ronlugge@gmail.com>
2015-07-20 11:06:18 -0700

    Removed mock.rb file in favor of new mock_server.rb file

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,5 +1,5 @@
-require 'montage_rails/mock'
+require 'montage_rails/mock_server'
 
 Rails.application.routes.draw do
-  mount MontageRails::Mock => "/montage_rails_mock"
+  mount MontageRails::MockServer => "/montage_rails_mock"
 end
